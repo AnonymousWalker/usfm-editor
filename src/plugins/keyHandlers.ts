976e34021647f15c877a285f7e2d7a74ec199ab3
Tony <hoanganhtran1998@gmail.com>
2025-10-23 14:36:19 -0400

    verse shortcut now uses the new add verse api

--- src/plugins/keyHandlers.ts
+++ src/plugins/keyHandlers.ts
@@ -4,7 +4,6 @@ import { MyTransforms } from "./helpers/MyTransforms"
 import { UsfmMarkers } from "../utils/UsfmMarkers"
 import { ReactEditor } from "slate-react"
 import { SelectionTransforms } from "./helpers/SelectionTransforms"
-import { emptyVerseWithVerseNumber, emptyInlineContainer } from "../transforms/basicSlateNodeFactory"
 import { VerseTransforms } from "./helpers/VerseTransforms"
 
 export function handleKeyPress(
@@ -225,6 +224,10 @@ export const withVerseShortcut = (editor: ReactEditor): ReactEditor => {
                     
                     // If either pattern matched, create the verse
                     if (verseNumber) {
+                                               
+                        // Use the refactored function to create the verse
+                        VerseTransforms.addVerseAtPoint(editor, selection.anchor, verseNumber)
+                        
                         // Delete the matched text
                         const deleteStart = offset - matchLength
                         Transforms.delete(editor, {
@@ -233,47 +236,6 @@ export const withVerseShortcut = (editor: ReactEditor): ReactEditor => {
                                 focus: { path: selection.anchor.path, offset: offset }
                             }
                         })
-                        
-                        // Insert new verse and split contents at the cursor
-                        const verseNodeEntry = MyEditor.getVerseNode(editor)
-                        if (verseNodeEntry && editor.selection) {
-                            const [_verse, versePath] = verseNodeEntry
-
-                            // Compute text before/after the cursor within the current verse
-                            const originalInlineContainerPath = versePath.concat(1)
-                            const [inlineContainer] = Editor.node(editor, originalInlineContainerPath)
-                            const fullVerseText = Node.string(inlineContainer)
-                            const verseStartPoint = Editor.start(editor, originalInlineContainerPath)
-                            const preRange = Editor.range(editor, verseStartPoint, editor.selection.anchor)
-                            const textBeforeCursor = Editor.string(editor, preRange)
-                            const textAfterCursor = fullVerseText.slice(textBeforeCursor.length)
-
-                            // Create and insert the new verse after the current one
-                            const newVerse = emptyVerseWithVerseNumber(verseNumber)
-                            const newVersePath = Path.next(versePath)
-                            Transforms.insertNodes(editor, newVerse, { at: newVersePath })
-
-                            // Reset both inline containers to empty
-                            const newInlineContainerPath = newVersePath.concat(1)
-                            MyTransforms.replaceNodes(editor, originalInlineContainerPath, emptyInlineContainer())
-                            MyTransforms.replaceNodes(editor, newInlineContainerPath, emptyInlineContainer())
-
-                            // Populate with split text
-                            MyTransforms.replaceText(
-                                editor,
-                                originalInlineContainerPath.concat(0),
-                                textBeforeCursor
-                            )
-                            MyTransforms.replaceText(
-                                editor,
-                                newInlineContainerPath.concat(0),
-                                textAfterCursor
-                            )
-
-                            // Place cursor at the start of the new verse content
-                            Transforms.select(editor, Editor.start(editor, newInlineContainerPath))
-                        }
-                        
                         return
                     }
                 }
