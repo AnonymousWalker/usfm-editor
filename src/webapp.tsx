730d853ba5af7728b749e6d4792a1b1d3eaaef60
Tony <hoanganhtran1998@gmail.com>
2025-10-27 14:37:19 -0400

    convert to function style

--- src/webapp.tsx
+++ src/webapp.tsx
@@ -13,36 +13,30 @@ const usfmString = `
 \\v 1 This is verse one
 `
 
-class Demo extends React.Component {
-    constructor(props: Record<string, never>) {
-        super(props)
-        this.handleEditorChange = this.handleEditorChange.bind(this)
-        this.state = { usfmOutput: usfmString }
+const Demo: React.FC = () => {
+    const [usfmOutput, setUsfmOutput] = React.useState(usfmString)
+
+    const handleEditorChange = (usfm: string) => {
+        setUsfmOutput(usfm)
     }
 
-    handleEditorChange(usfm: string) {
-        this.setState({ usfmOutput: usfm })
-    }
-
-    render() {
-        return (
-            <div className="row">
-                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-                    {/* The editor can be given a ref of type UsfmEditorRef
-                     to have access to the editor API (use React.createRef<UsfmEditorRef>)
-                    */}
-                    <UsfmEditor
-                        usfmString={usfmString}
-                        onChange={this.handleEditorChange}
-                        toolbarSpecs={defaultToolbarSpecs}
-                    />
-                </div>
-                <div style={{ display: "none" }}>
-                    <OutputUsfm usfm={(this.state as { usfmOutput: string }).usfmOutput} />
-                </div>
+    return (
+        <div className="row">
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                {/* The editor can be given a ref of type UsfmEditorRef
+                 to have access to the editor API (use React.createRef<UsfmEditorRef>)
+                */}
+                <UsfmEditor
+                    usfmString={usfmString}
+                    onChange={handleEditorChange}
+                    toolbarSpecs={defaultToolbarSpecs}
+                />
             </div>
-        )
-    }
+            <div style={{ display: "none" }}>
+                <OutputUsfm usfm={usfmOutput} />
+            </div>
+        </div>
+    )
 }
 
 const container = document.getElementById("root")
