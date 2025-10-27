d53cb16ad44b0074676cbf8748c92f7f53981ddb
Tony <hoanganhtran1998@gmail.com>
2025-10-27 14:50:12 -0400

    update UI layout

--- src/webapp.tsx
+++ src/webapp.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
+import { Container, Box } from "@material-ui/core"
 import "./style.css"
 import "./demo/demo.css"
 import { UsfmEditor } from "./index"
@@ -11,6 +12,8 @@ const usfmString = `
 \\c 1
 \\p
 \\v 1 This is verse one
+\\p Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+\\p Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi turpis sem, placerat eu condimentum id, pellentesque ut magna. Praesent dignissim arcu ac libero ultrices dapibus.
 `
 
 const Demo: React.FC = () => {
@@ -21,21 +24,25 @@ const Demo: React.FC = () => {
     }
 
     return (
-        <div className="row">
-            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-                {/* The editor can be given a ref of type UsfmEditorRef
-                 to have access to the editor API (use React.createRef<UsfmEditorRef>)
-                */}
-                <UsfmEditor
-                    usfmString={usfmString}
-                    onChange={handleEditorChange}
-                    toolbarSpecs={defaultToolbarSpecs}
-                />
-            </div>
-            <div style={{ display: "none" }}>
-                <OutputUsfm usfm={usfmOutput} />
-            </div>
-        </div>
+        <Container maxWidth="md" style={{ fontFamily: 'Segoe UI, Tahoma, Geneva, Verdana, sans-serif' }}>
+            <Box py={4}>
+                <div className="row">
+                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', lineHeight: '1.5' }}>
+                        {/* The editor can be given a ref of type UsfmEditorRef
+                         to have access to the editor API (use React.createRef<UsfmEditorRef>)
+                        */}
+                        <UsfmEditor
+                            usfmString={usfmString}
+                            onChange={handleEditorChange}
+                            toolbarSpecs={defaultToolbarSpecs}
+                        />
+                    </div>
+                    <div style={{ display: "none" }}>
+                        <OutputUsfm usfm={usfmOutput} />
+                    </div>
+                </div>
+            </Box>
+        </Container>
     )
 }
 
